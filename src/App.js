29af74bcabdeebbf9b5c5bb18b350ad23055c68f
Bhavik Gosrani <bgosrani@guidepoint.com>
2019-09-21 18:59:07 -0400

    Adding state to class component and click event

--- src/App.js
+++ src/App.js
@@ -4,13 +4,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    Person: [
+      { name: "Mr B.", age: "34" },
+      { name: "Mrs P", age: "30" },
+      { name: "Mr A", age: "27" }
+    ]
+  }
+
+  switchNameHandler=()=>{
+    console.log('was clicked');
+
+    this.setState({
+      Person: [
+        { name: "Mr Bhavik", age: "34" },
+        { name: "Mrs P", age: "30" },
+        { name: "Mr A", age: "27" }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1> Hi Welcome to React App!!!</h1>
-        <Person name="Bhavik" age="34" />
-        <Person name="Pooja" age="30" />
-        <Person name="Arpan" age="27"> I am Pooja's brother</Person>
+        <button onClick={this.switchNameHandler}>switch name</button>
+        <Person name={this.state.Person[0].name} age={this.state.Person[0].age} />
+        <Person name={this.state.Person[1].name} age={this.state.Person[1].age} />
+        <Person name={this.state.Person[2].name} age={this.state.Person[2].age} > This is P's bro </Person >
       </div>
     );
   }
