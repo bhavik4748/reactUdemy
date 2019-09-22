903d4407e8e04f9ed146d4455f323a39a02b33a0
Bhavik Gosrani <bgosrani@guidepoint.com>
2019-09-21 23:01:47 -0400

    adding events and passing data

--- src/App.js
+++ src/App.js
@@ -13,12 +13,22 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler=()=>{
-    console.log('was clicked');
+  switchNameHandler = (newName) => {
+    // console.log('was clicked');
 
     this.setState({
       Person: [
-        { name: "Mr Bhavik", age: "34" },
+        { name: newName, age: "34" },
+        { name: "Mrs P", age: "30" },
+        { name: "Mr A", age: "27" }
+      ]
+    })
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      Person: [
+        { name: event.target.value, age: "34" },
         { name: "Mrs P", age: "30" },
         { name: "Mr A", age: "27" }
       ]
@@ -29,10 +39,10 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Hi Welcome to React App!!!</h1>
-        <button onClick={this.switchNameHandler}>switch name</button>
+        <button onClick={() => this.switchNameHandler()}>switch name</button>
         <Person name={this.state.Person[0].name} age={this.state.Person[0].age} />
         <Person name={this.state.Person[1].name} age={this.state.Person[1].age} />
-        <Person name={this.state.Person[2].name} age={this.state.Person[2].age} > This is P's bro </Person >
+        <Person click={this.switchNameHandler.bind(this, "Bhav!")} name={this.state.Person[2].name} age={this.state.Person[2].age} change={this.nameChangedHandler}> This is P's bro </Person >
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,9 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>I am {props.name} and I am {props.age}</p>
+            <p onClick={props.click}>I am {props.name} and I am {props.age}</p>
             <p>{props.children}</p>
+            <p><input type="text" onChange={props.change} ></input></p>
         </div>
     )
 
