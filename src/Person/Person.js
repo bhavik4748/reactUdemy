27f76e213cd777e57b1f48d026e08aacdbfdf19f
Bhavik Gosrani <bhavik4748@gmail.com>
2019-09-25 08:23:42 -0400

    fixed two way binding
    fixed two way binding

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,12 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p onClick={props.click}>I am {props.name} and I am {props.age}</p>
+            <p onClick={props.click}>
+                I am {props.name} and I am {props.age}
+            </p>
             <p>{props.children}</p>
             <p>
-                <input type="text" onChange={props.changed}  value={props.name} >
+                <input type="text" onChange={props.changed} value={props.name} >
                 </input>
             </p>
         </div>
